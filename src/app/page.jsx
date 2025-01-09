ff8caf1e79d46194e1959b34a86b67ed9a2254ec
salman24823 <salman248shah@gmail.com>
2025-01-09 13:04:30 +0500

    asd

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,6 @@ import ContactForm from "./Sections/Contact-Form/page";
 import Features from "./Sections/Feature-Section/page";
 import Hero from "./Sections/Hero-Section/page";
 import NewsLetter from "./Sections/NewsLetter/page";
-import Portfolio from "./Sections/Portfolio/page";
 import ServiceCards from "./Sections/Service-Cards/page";
 
 export default function Home() {
