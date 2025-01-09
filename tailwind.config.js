c4e9e4d856a9eca8c25b2d51cf81b15c9184f5fe
salman24823 <salman248shah@gmail.com>
2025-01-09 13:08:00 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 import { nextui } from '@nextui-org/react';
+import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
@@ -31,7 +28,9 @@ module.exports = {
     },
   },
   plugins: [
-    require('daisyui'),
+    daisyui,
     nextui(),
   ],
 };
+
+export default config;
