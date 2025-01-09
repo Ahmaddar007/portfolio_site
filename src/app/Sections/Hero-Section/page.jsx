fb99ac92719a43b90ce3a4c932203dee58048593
salman24823 <salman248shah@gmail.com>
2025-01-09 13:50:12 +0500

    Update page.jsx

--- src/app/Sections/Hero-Section/page.jsx
+++ src/app/Sections/Hero-Section/page.jsx
@@ -22,13 +22,15 @@ const Hero = () => {
 
     <div className="flex relative justify-center items-center">
 
-      <div className='border absolute mt-5 border-green-500 justify-center items-center flex'>
+      <div className=' absolute mt-5 justify-center items-center flex'>
         <img className='z-20 text-blue-500 h-auto w-3/4' src="https://utfs.io/f/vm2okaME29juPLFLxSyTd6rYMlUazXEvVy5HbfupxD9mPG4i" />
         <img className='absolute -top-52 -left-32  z-20 text-blue-500 h-auto w-52' src="https://utfs.io/f/vm2okaME29jujXBrzo30jh5ZniMeLTuItDbWrUYSP9pQloEx" />
         <img className='absolute -top-48 right-0  z-20 text-blue-500 h-auto w-28' src="https://utfs.io/f/vm2okaME29juWyYWROfTcLyZDikjAtpxHUVG1hRvJma54gOQ" />
+        <img className='absolute -bottom-36 -left-16  z-20 text-blue-500 h-auto w-28' src="https://utfs.io/f/vm2okaME29jugD62rkFvp84ItSReTN70WyCdXDYm6raFOQnZ" />
+        <img className='absolute -bottom-40 -right-16  z-20 text-blue-500 h-auto w-28' src="https://utfs.io/f/vm2okaME29jue6kQ1cmy38BTF1VR6JD7n2fXHwLbmruoUCMk" />
       </div>
 
-      <img className='top-0 border border-red-500 w-3/4' src="https://utfs.io/f/vm2okaME29juuqZlNVgjyJeHg1tY6R4mbkVlD2EFOcZrNILS" />
+      <img className='top-0  w-3/4' src="https://utfs.io/f/vm2okaME29juuqZlNVgjyJeHg1tY6R4mbkVlD2EFOcZrNILS" />
 
     </div>
 
