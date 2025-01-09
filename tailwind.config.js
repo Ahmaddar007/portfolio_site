8f116a478cb030af87ec338f1b9a28f1d5a3e7ef
salman24823 <salman248shah@gmail.com>
2025-01-09 13:02:03 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,37 @@
 import { nextui } from '@nextui-org/react';
 
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-
-  './pages/**/*.{js,ts,jsx,tsx}',
-  './components/**/*.{js,ts,jsx,tsx}',
-
-  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-];
-export const theme = {
-  extend: {
-    fontFamily: {
-      inter: ['Inter', 'sans-serif'],
-    },
-    animation: {
-      'infinite-scroll': 'infinite-scroll 25s linear infinite',
-    },
-    keyframes: {
-      'infinite-scroll': {
-        '0%': { transform: 'translateX(0)' },
-        '100%': { transform: 'translateX(-100%)' },
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
-    colors: {
-      background: "var(--background)",
-      foreground: "var(--foreground)",
-    },
   },
+  plugins: [
+    require('daisyui'),
+    nextui(),
+  ],
 };
-export const plugins = [
-  require('daisyui'),
-  nextui()
-];
