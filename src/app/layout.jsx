7ecfcebd09b96c53bd307c0f0b00848534f5931e
salman24823 <salman248shah@gmail.com>
2025-01-25 16:14:11 +0500

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -24,9 +24,9 @@ export default function RootLayout({ children }) {
         <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Lora:ital,wght@0,400..700;1,400..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Satisfy&display=swap" rel="stylesheet"></link>
 
       </head>
-      <body>
+      <body className="overflow-x-hidden">
         <NextUIProvider>
-          <div className="flex overflow-hidden flex-col items-center">
+          <div className="flex flex-col items-center">
             <Header />
             {children}
             <Footer />
