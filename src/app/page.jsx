403414a8bffb2332b9de9a2cd33657c0aaf971b0
salman24823 <salman248shah@gmail.com>
2025-02-25 20:29:38 +0500

    ASD

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,10 @@
 "use client"
 
-import Customize_Section from "./components/Customize_section/page";
 import Faqs from "./components/Faqs/page";
 import LandingSlider from "./components/landingSlider/page";
 
 import ContactForm from "./Sections/Contact-Form/page";
-import Features from "./Sections/Feature-Section/page";
+// import Features from "./Sections/Feature-Section/page";
 import Hero from "./Sections/Hero-Section/page";
 import NewsLetter from "./Sections/NewsLetter/page";
 import ServiceCards from "./Sections/Service-Cards/page";
@@ -23,7 +22,7 @@ export default function Home() {
    <Newarrival />
    <Trending_items />
    <ServiceCards />
-   <Customize_Section />
+   {/* <Customize_Section /> */}
    <Faqs />
    <ContactForm />
    <NewsLetter />
